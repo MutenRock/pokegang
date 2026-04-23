c5dfec06c9da79bedfd7424b8066d6043eac4564
MutenRock <pierrehoyaux@hotmail.fr>
2026-04-23 13:21:29 +0200

    Fix dex helper import and species name maps setup

--- game/data/dex-helpers.js
+++ game/data/dex-helpers.js
@@ -5,7 +5,7 @@ import { POKEDEX_DESC } from './pokedex-desc.js';
 const TYPE_DESC_FR = {
   Fire:'Type Feu, maîtrise les flammes.',
   Water:'Type Eau, vit dans les milieux aquatiques.',
-  Grass:'Type Plante, absorbe l'énergie solaire.',
+  Grass:'Type Plante, absorbe l\'énergie solaire.',
   Electric:'Type Électrik, génère des charges électriques.',
   Psychic:'Type Psy, possède des pouvoirs mentaux.',
   Ice:'Type Glace, résiste aux températures extrêmes.',
@@ -14,7 +14,7 @@ const TYPE_DESC_FR = {
   Fighting:'Type Combat, maîtrise les arts martiaux.',
   Poison:'Type Poison, sécrète des toxines dangereuses.',
   Ground:'Type Sol, creuse et se déplace sous terre.',
-  Flying:'Type Vol, plane sur les courants d'air.',
+  Flying:'Type Vol, plane sur les courants d\'air.',
   Bug:'Type Insecte, pullule dans la végétation.',
   Rock:'Type Roche, son corps est aussi dur que la pierre.',
   Ghost:'Type Spectre, insaisissable et mystérieux.',
@@ -27,14 +27,14 @@ function getDexDesc(species_en, speciesByEn = globalThis.SPECIES_BY_EN) {
   return TYPE_DESC_FR[sp.types?.[0]] || 'Un Pokémon aux capacités encore peu connues.';
 }
 
-function buildSpeciesNameMaps(speciesList) {
+function buildSpeciesNameMaps(speciesList = []) {
   const FR_TO_EN = {};
   const EN_TO_FR = {};
-  (speciesList || []).forEach(s => {
+  for (const s of speciesList || []) {
     FR_TO_EN[s.fr.toLowerCase()] = s.en;
     EN_TO_FR[s.en] = s.fr;
-  });
+  }
   return { FR_TO_EN, EN_TO_FR };
 }
 
-export { getDexDesc, buildSpeciesNameMaps };
+export { TYPE_DESC_FR, getDexDesc, buildSpeciesNameMaps };
